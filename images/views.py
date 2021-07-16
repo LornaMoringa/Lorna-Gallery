e85b467d9721281f269dbf82c0977a2ff297dfb1
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-16 12:14:28 +0300

    Adding search view

--- images/views.py
+++ images/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Photo,Location,Category
+from .models import Image,Location,Category
 from django.http import HttpResponse,Http404
 from django.core.exceptions import ObjectDoesNotExist
 import datetime as dt
 
 # Create your views here.
 def index(request):
-  images =Photo.objects.all()
+  images =Image.objects.all()
   location = Location.objects.all()
   return render(request,'index.html',{'images':images, 'location':location})
+
+def search_images(request):
+  if 'image' in request.GET and request.GET["image"]:
+    search_term = request.GET.get("image")
+    searched_images = Image.find_category(search_term)
+    message = f"{search_term}"
+
+    return render(request, 'search.html', {"message":message,"images":searched_images})
+
+  else:
+    message = 'You have not searched for any term'
+    return render(request, 'search.html', {"message":message})

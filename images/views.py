93fd98b54edc60c676f1c2d7d03495cffbf3ad8a
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-16 12:10:14 +0300

    Adding index view

--- images/views.py
+++ images/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Photo,Location,Category
+from django.http import HttpResponse,Http404
+from django.core.exceptions import ObjectDoesNotExist
+import datetime as dt
 
 # Create your views here.
+def index(request):
+  images =Photo.objects.all()
+  location = Location.objects.all()
+  return render(request,'index.html',{'images':images, 'location':location})

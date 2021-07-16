58648f4d247c6689cc3c57ebaa7c3b4413df4bb0
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-16 11:30:44 +0300

    Adding location class into models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Location(models.Model):
+  name = models.CharField(max_length =50)

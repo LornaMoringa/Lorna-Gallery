8d4757e1cd82392e47514d25c435cde2acc99380
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-16 12:04:40 +0300

    updating app urls

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-#   path('',views.home,name='homePage'),
+  path('',views.index,name='home'),
+  path('school/',views.school_images,name='school'),
+  path('nairobi/',views.nairobi_images,name='nairobi'),
+  path('coast/',views.coast_images,name='coast'),
+  path('kericho/',views.kericho_images,name='kericho'),
+  path('image_details/<int:image_id>',views.detail_details,name='image.detail'),
+  path('search_category/',views.search_category_images, name='search_images')
 ]
 
 if settings.DEBUG:
